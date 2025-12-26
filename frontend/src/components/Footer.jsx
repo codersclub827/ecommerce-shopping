2b5787e0cd6174f6b50ab204b314e2cfaa223c15
codersclub827 <dishajoshi0611@gmail.com>
2025-12-26 22:40:40 +0530

    Update contact information in Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -25,8 +25,8 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li>+880 1992113015</li>
-            <li>pranto113015@gmail.com</li>
+            <li>+8830370331</li>
+            <li>dishajoshi0611@gmail.com</li>
           </ul>
         </div>
       </div>
